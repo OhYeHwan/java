83ed8065fbd6dcf8797ae331e3a301319346055e
OOHYEE <azss0470@gmail.com>
2021-08-13 11:04:48 +0900

    [Lamba & stream] 람다식의 타입과 형변환

--- ch14/Ex14_2.java
+++ ch14/Ex14_2.java
@@ -1,2 +1,23 @@
-package ch14;public class Ex14_2 {
+package ch14;
+
+@FunctionalInterface
+interface MyFunction2 {
+    void myMethod();
+}
+
+public class Ex14_2 {
+    public static void main(String[] args) {
+        MyFunction2 f = () -> {}; // MyFunction2 f = (Myfunction2)(()->{});
+        Object obj = (MyFunction2)(() -> {}); // Object 타입으로 형변환이 생략됨
+        String str = ((Object)(MyFunction2)(() -> {})).toString();
+
+        System.out.println(f);
+        System.out.println(obj);
+        System.out.println(str);
+
+//        System.out.println(()->{}); // 에러. 람다식은 Object타입으로 형변환 안됨
+        System.out.println((MyFunction2)(()->{}));
+//        System.out.println((MyFunction2)(()->{})).toString(); // 에러
+        System.out.println(((Object)(MyFunction2)(() -> {})).toString());
+    }
 }
